b68c2d3a453c3e5e60b132a0bf6427249b906a34
James Montalvo <edwin.j.montalvo@nasa.gov>
2014-07-08 10:07:29 -0500

    clean up post-process code

--- wikify.php
+++ wikify.php
@@ -11,74 +11,43 @@ class PostProcess {
 		// remove weird question marks
 		$new_text = str_replace("�", "", $new_text);
 		
-		$patterns = array();
-		$replaces = array();
-		
-		$patterns[0] = "/<\/font>/i";
-		$replaces[0] = "";
+		$replaces = array(
+			"/<\/font>/i" => "",
+			
+			"/<font[^>]+>/i" => "",
 
-		$patterns[1] = "/<font[^>]+>/i";
-		$replaces[1] = "";
+			"/(\[\/INTERNAL-WIKI-LINK)(\S*)(\s+)([^\]]*)(\])/" => '[[\4]]',
 
-		// $patterns[2] = "/(\[/INTERNAL-WIKI-LINK)([^\s*])(\s)([^\]*])(\])/";
-		// $replaces[2] = "[[\4]]";
-		$patterns[2] = "/(\[\/INTERNAL-WIKI-LINK)(\S*)(\s+)([^\]]*)(\])/";
-		$replaces[2] = '[[\4]]';
+			"/%20/" => " ",
+			
+			// match /style="..."/ where "..."=anything except double quotes
+			'/style="[^\"]*"/i' => "",
 
-		$patterns[3] = "/%20/";
-		$replaces[3] = " ";
-		
-		$patterns[4] = '/style="[^\"]*"/i'; // match /style="..."/ where "..."=anything except double quotes
-		$replaces[4] = "";
+			// match {|
+			'/{\|/' => "{| class=\"wikitable\" ",
 
-		$patterns[5] = '/{\|/'; // match {|
-		$replaces[5] = "{| class=\"wikitable\" ";
+			// remove single lines of bold text, where text less than 60 characters
+			"/(?:^|\n)\s*[\<u>]*(?:'''|''\s*'''|'''\s*'')\s*([^\n^']{0,60})(?:'''|''\s*'''|'''\s*'')[<\/u>]*\s*\n/i" =>
+			"\n\n" . '=== \1 ===' . "\n", // replace with level 3 heading
 
-		// remove single lines of bold text, where text less than 60 characters
-		$patterns[6] = "/(?:^|\n)\s*[\<u>]*(?:'''|''\s*'''|'''\s*'')\s*([^\n^']{0,60})(?:'''|''\s*'''|'''\s*'')[<\/u>]*\s*\n/i";
-		$replaces[6] = "\n\n" . '=== \1 ===' . "\n"; // replace with level 3 heading
+			"/\n[^\S\r\n]+/u" => "\n",		
+			
+			"/(\r?\n){4,}/" => "\n\n\n",
 
-		$patterns[7] = "/\n[^\S\r\n]+/u"; 
-		$replaces[7] = "\n";		
-		
-		$patterns[8] = "/(\r?\n){4,}/"; 
-		$replaces[8] = "\n\n\n";
+			"/&amp;/" => "&",
+			
+			"/[^\S\r\n]{2,}/u" => ' ',
 
-		$patterns[9] = "/&amp;/"; 
-		$replaces[9] = "&";
-		
-		// $patterns[9] = "/[^\S\r\n]{20}/"; 
-		// $replaces[9] = ' ';
-		
-		// $patterns[6] = "/\n{3,20}/"; 
-		// $replaces[6] = "\n\n"; // replace with level 3 heading
-		
-		$new_text = preg_replace($patterns, $replaces, $new_text);
-		
-		// echo "<h1>Old Text</h1>";
-		// echo "<textarea type='text' style='width:600px;height:300px;'>$old_text</textarea>";
+			"/·/u" => '*',
+			
+			"/<\/span>/i" => "",
 
-		// echo "<h1>New Text</h1>";
-		// echo "<textarea type='text' style='width:600px;height:300px;'>$new_text</textarea>";
-		$patterns = array();
-		$replaces = array();
+			"/<span[^>]+>/i" => "",
 
-		$patterns[0] = "/[^\S\r\n]{2,}/u"; 
-		// $patterns[0] = "/\s{2,}/u"; 
-		$replaces[0] = ' ';
-
-		$patterns[1] = "/·/u";
-		$replaces[1] = '*';
-		
-		$patterns[2] = "/<\/span>/i";
-		$replaces[2] = "";
-
-		$patterns[3] = "/<span[^>]+>/i";
-		$replaces[3] = "";
-
-		$new_text = preg_replace($patterns, $replaces, $new_text);
-
-		
+		);
+				
+		$new_text = preg_replace(array_keys($replaces), array_values($replaces), $new_text);
+				
 		return $new_text;
 		
 	}
