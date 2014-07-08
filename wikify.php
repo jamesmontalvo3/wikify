4328bb7dd2105bb3da5c1a620d82c18497b76bca
James Montalvo <edwin.j.montalvo@nasa.gov>
2014-07-08 13:42:36 -0500

    fix tag stripping; add unicode char conversions; allow keeping files
    1) Fix tag stripping: font, span and div tags were only being removed
       if they included something after the tag name, e.g. <div style="">
       they were being removed, but if they did not, e.g. <div>, then
       they were not. This is fixed.
    
    2) Add unicode char conversions: Converts wingdings characters to
       the closest available unicode characters. This is NOT all unicode
       characters by any means.
    
    3) Allow keeping files: by adding the --keepfiles option users can
       choose to not delete the /tmp directory with intermediate HTML
       and .wiki files

--- wikify.php
+++ wikify.php
@@ -10,11 +10,11 @@ class PostProcess {
 		
 		// remove weird question marks
 		$new_text = str_replace("�", "", $new_text);
-		
+				
 		$replaces = array(
 			"/<\/font>/i" => "",
 			
-			"/<font[^>]+>/i" => "",
+			"/<font[^>]*>/i" => "",
 
 			"/(\[\/INTERNAL-WIKI-LINK)(\S*)(\s+)([^\]]*)(\])/" => '[[\4]]',
 
@@ -42,7 +42,11 @@ class PostProcess {
 			
 			"/<\/span>/i" => "",
 
-			"/<span[^>]+>/i" => "",
+			"/<span[^>]*>/i" => "",
+
+			"/<\/div>/i" => "",
+
+			"/<div[^>]*>/i" => "",
 
 		);
 				
@@ -52,12 +56,105 @@ class PostProcess {
 		
 	}
 
+	static public function wingdings_to_unicode ($text) {
+
+		$conversions = array(
+
+			// right arrow
+			"&#61664;" => "&#8680;",
+			"&#61672;" => "&#8680;",
+			"&#61680;" => "&#8680;",
+
+			// left arrow
+			"&#61663;" => "&#8678;",
+			"&#61671;" => "&#8678;",
+			"&#61679;" => "&#8678;",
+
+			// up arrow
+			"&#61665;" => "&#8679;",
+			"&#61673;" => "&#8679;",
+			"&#61681;" => "&#8679;",
+
+			// down arrow
+			"&#61666;" => "&#8681;",
+			"&#61674;" => "&#8681;",
+			"&#61682;" => "&#8681;",
+
+			// NW arrow
+			"&#61667;" => "&#11009;",
+			"&#61685;" => "&#11009;",
+			"&#61675;" => "&#11009;",
+
+			// NE arrow
+			"&#61668;" => "&#11008;",
+			"&#61676;" => "&#11008;",
+			"&#61686;" => "&#11008;",
+
+			// SW arrow
+			"&#61669;" => "&#11011;",
+			"&#61677;" => "&#11011;",
+			"&#61687;" => "&#11011;",
+
+			// SE arrow
+			"&#61670;" => "&#11010;",
+			"&#61678;" => "&#11010;",
+			"&#61688;" => "&#11010;",
+
+			// right-left arrow
+			"&#61683;" => "&#11012;",
+
+			// up-down arrow
+			"&#61684;" => "&#8691;",
+
+			// checkmark
+			"&#61692;" => "&#10003;",
+
+			// x-mark;",
+			"&#61691;" => "&#10007;",
+
+			// x-box;",
+			"&#61693;" => "&#9746;",
+
+			// checked-box;",
+			"&#61694;" => "&#9745;",
+			
+		);
+
+		foreach($conversions as $wingding => $unicode) {
+			$text = str_replace($wingding, $unicode, $text);
+		}
+		
+		return $text;
+		
+	}
+	
 }
+
+// defaults:
+$keepfiles = false;
+
 
 
 $soffice = "C:/Program Files (x86)/LibreOffice 4/program/soffice.exe";
 
-$file = trim( $argv[1] );
+if ( count($argv) < 2 ) {
+	die("please specify filename");
+}
+$file_arg_index = count($argv) - 1;
+
+// don't start with zero (is just script name)
+// don't go to final arg, since that's the file to be wikified
+for($i=1; $i<$file_arg_index; $i++) {
+	switch(trim($argv[$i])) {
+		case "--keepfiles":
+			$keepfiles = true;
+			break;
+		default:
+			echo "\nOption " . trim($argv[$i]) . " is not valid";
+	}
+}
+
+$file = trim( $argv[$file_arg_index] );
 
 // if file doesn't start with a letter and a colon (i.e. a full path with drive letter) 
 if ( preg_match('/[^\s]\:/', $file) == 0 ) {
@@ -83,9 +180,13 @@ mkdir($tmp);
 shell_exec("\"$soffice\" --headless --convert-to html:HTML -outdir \"$tmp\" \"$file\"");
 
 $html_file = $tmp . '/' . $filebase . '.html';
+$new_html_file = $tmp . '/' . $filebase . '.unicode.html';
+
+file_put_contents($new_html_file, PostProcess::wingdings_to_unicode(file_get_contents($html_file)) );
+
 $initial_wiki_file = $tmp . '/' . $filebase . '.wiki';
 
-shell_exec("html2wiki --dialect MediaWiki \"$html_file\" > \"$initial_wiki_file\"");
+shell_exec("html2wiki --dialect MediaWiki \"$new_html_file\" > \"$initial_wiki_file\"");
 
 $final_file = $dir . '/' . $filebase . '.wiki';
 
@@ -94,6 +195,15 @@ file_put_contents(
 	PostProcess::process(file_get_contents($initial_wiki_file))
 );
 
-unlink($html_file);
-unlink($initial_wiki_file);
-rmdir($tmp);
+if ( ! $keepfiles ) {
+	unlink($html_file);
+	unlink($new_html_file);
+	unlink($initial_wiki_file);
+	rmdir($tmp);
+}
+
+if ( file_exists($tmp) ) {
+	$tmp_time = $tmp . '_' . $filebase . '_' . time();
+	rename($tmp, $tmp_time);
+	echo "\"$tmp\" could not be removed. Renamed to \"$tmp_time\"";
+}
