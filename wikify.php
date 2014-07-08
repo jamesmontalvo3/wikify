4cb2aff2ccdf5a9b90e5f8a5c653f66cdf9dc766
James Montalvo <edwin.j.montalvo@nasa.gov>
2014-07-08 09:47:37 -0500

    improve post-processing; input file from command line arg

--- wikify.php
+++ wikify.php
@@ -11,10 +11,13 @@ class PostProcess {
 		// remove weird question marks
 		$new_text = str_replace("�", "", $new_text);
 		
-		$patterns[0] = "/<\/font>/";
+		$patterns = array();
+		$replaces = array();
+		
+		$patterns[0] = "/<\/font>/i";
 		$replaces[0] = "";
 
-		$patterns[1] = "/<font[^>]+>/";
+		$patterns[1] = "/<font[^>]+>/i";
 		$replaces[1] = "";
 
 		// $patterns[2] = "/(\[/INTERNAL-WIKI-LINK)([^\s*])(\s)([^\]*])(\])/";
@@ -25,16 +28,30 @@ class PostProcess {
 		$patterns[3] = "/%20/";
 		$replaces[3] = " ";
 		
-		$patterns[4] = '/style="[^\"]*"/'; // match /style="..."/ where "..."=anything except double quotes
+		$patterns[4] = '/style="[^\"]*"/i'; // match /style="..."/ where "..."=anything except double quotes
 		$replaces[4] = "";
 
 		$patterns[5] = '/{\|/'; // match {|
 		$replaces[5] = "{| class=\"wikitable\" ";
 
 		// remove single lines of bold text, where text less than 60 characters
-		$patterns[6] = "/(^|\n)(''')(\s*)([^\n^']{0,60})(''')(\s*)(\n|)/"; 
-		$replaces[6] = "\n" . '=== \4 ===' . "\n"; // replace with level 3 heading
+		$patterns[6] = "/(?:^|\n)\s*[\<u>]*(?:'''|''\s*'''|'''\s*'')\s*([^\n^']{0,60})(?:'''|''\s*'''|'''\s*'')[<\/u>]*\s*\n/i";
+		$replaces[6] = "\n\n" . '=== \1 ===' . "\n"; // replace with level 3 heading
 
+		$patterns[7] = "/\n[^\S\r\n]+/u"; 
+		$replaces[7] = "\n";		
+		
+		$patterns[8] = "/(\r?\n){4,}/"; 
+		$replaces[8] = "\n\n\n";
+
+		$patterns[9] = "/&amp;/"; 
+		$replaces[9] = "&";
+		
+		// $patterns[9] = "/[^\S\r\n]{20}/"; 
+		// $replaces[9] = ' ';
+		
+		// $patterns[6] = "/\n{3,20}/"; 
+		// $replaces[6] = "\n\n"; // replace with level 3 heading
 		
 		$new_text = preg_replace($patterns, $replaces, $new_text);
 		
@@ -43,7 +60,25 @@ class PostProcess {
 
 		// echo "<h1>New Text</h1>";
 		// echo "<textarea type='text' style='width:600px;height:300px;'>$new_text</textarea>";
+		$patterns = array();
+		$replaces = array();
 
+		$patterns[0] = "/[^\S\r\n]{2,}/u"; 
+		// $patterns[0] = "/\s{2,}/u"; 
+		$replaces[0] = ' ';
+
+		$patterns[1] = "/·/u";
+		$replaces[1] = '*';
+		
+		$patterns[2] = "/<\/span>/i";
+		$replaces[2] = "";
+
+		$patterns[3] = "/<span[^>]+>/i";
+		$replaces[3] = "";
+
+		$new_text = preg_replace($patterns, $replaces, $new_text);
+
+		
 		return $new_text;
 		
 	}
@@ -53,9 +88,7 @@ class PostProcess {
 
 $soffice = "C:/Program Files (x86)/LibreOffice 4/program/soffice.exe";
 
-echo "file: ";
-$handle = fopen ("php://stdin","r");
-$file = trim( fgets($handle) );
+$file = trim( $argv[1] );
 
 // if file doesn't start with a letter and a colon (i.e. a full path with drive letter) 
 if ( preg_match('/[^\s]\:/', $file) == 0 ) {
